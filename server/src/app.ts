ddc55edad0a917a8615952dfcdfd144d2d56184b
Excersise_tracking <apekshamorappanavar09@gmail.com>
2026-07-06 19:41:42 +0530

    Handled CORS

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,7 +14,7 @@ import accountRoutes from "./routes/account";
 import recommendationRoutes from "./routes/recommendations";
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "*" }));
+app.use(cors({ origin: process.env.CLIENT_URL || "*", credentials: true }));
 app.use(express.json());
 
 app.use(
